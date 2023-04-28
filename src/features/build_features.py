6b361c06e5fd403fc6744ab09869f4becf2e0b93
Bertan ULUSOY <bertan_ulusoy@yahoo.com>
2023-04-28 05:46:12 +0300

    Custom dataset's data loader function created.

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,4 +1,6 @@
 import torch
+from torch.utils.data import Dataset
+from image_captioning_dataset import ImageCaptioningDataset
 
 
 def collate_function(data_batch):
@@ -30,3 +32,20 @@ def collate_function(data_batch):
     return images, targets, caption_lengths
 
 
+def get_loader(data_path, coco_json_path, vocabulary, transform, batch_size, shuffle, num_workers):
+    """Returns torch.utils.data.DataLoader for custom coco dataset."""
+    # COCO Image Captioning Dataset
+    coco_dataset = ImageCaptioningDataset(data_path=data_path,
+                                          coco_json_path=coco_json_path,
+                                          vocabulary=vocabulary,
+                                          transform=transform)
+    # This will return (images, captions, lengths) for each iteration.
+    # images: a tensor of shape (batch_size, 3, 224, 224).
+    # captions: a tensor of shape (batch_size, padded_length)
+    # lengths: a list indicating valid length for each caption. length is (batch_size).
+    custom_data_loader = torch.utils.data.DataLoader(dataset=coco_dataset,
+                                                     batch_size=batch_size,
+                                                     shuffle=shuffle,
+                                                     num_workers=num_workers,
+                                                     collate_fn=collate_function)
+    return custom_data_loader
